ec8b401f37d360b1c2926ebc7df5e94d4abfaaad
Gustavo Lozada <zelote57@gmail.com>
2024-01-12 21:30:40 -0500

    Add project files.

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicAPI.Models;
+
+namespace MusicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private static List<Song> _songs = new List<Song>
+        {
+            new Song{ 
+                Id = 1, 
+                Title = "Yeah", 
+                Language = "English"
+            },
+            new Song
+            {
+                Id = 2,
+                Title = "Still Waiting",
+                Language = "English"
+            },
+            new Song
+            {
+                Id = 3,
+                Title = "Entre Nosotros",
+                Language = "Spanish"
+            }
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> GetAllSongs()
+        {
+            return _songs;
+        }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicAPI.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Language { get; set; }
+    }
+}
