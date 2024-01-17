bc5cb365ff4b942c0335691996767da4a076db22
Gustavo Lozada <zelote57@gmail.com>
2024-01-16 21:03:08 -0500

    add complete crud without db persistence

--- Controllers/SongsController.cs
+++ Controllers/SongsController.cs
@@ -35,10 +35,48 @@ namespace MusicAPI.Controllers
             return _songs;
         }
 
+        //GET api/Songs/0
+        [HttpGet("{id}")]
+        public Song GetSongById(int id)
+        {
+            return _songs.Find(song => song.Id == id);
+        }
+
         [HttpPost]
         public IActionResult SaveSong([FromBody] Song newSong)
         {
             _songs.Add(newSong);
+            return Ok();
+        }
+
+        //PUT api/Songs/0/Abc
+        [HttpPut("{id}/{newTitle}")]
+        public IActionResult UpdateSongTitle(int id, string newTitle)
+        {
+            var song = _songs.Find(song => song.Id == id);
+            song.Title = newTitle;
+
+            return Ok();
+        }
+
+        //PUT api/Songs/2
+        [HttpPut("{id}")]
+        public IActionResult UpdateSong(int id,[FromBody] Song updatedSong)
+        {
+            var song = _songs.Find(song => song.Id == id);
+            
+            song.Title = updatedSong.Title;
+            song.Language = updatedSong.Language;
+
+            return Ok();
+        }
+
+        //DELETE api/Songs/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSong(int id)
+        {
+            var song = _songs.Find(song => song.Id == id);
+            _songs.Remove(song);
             return Ok();
         }
     }
