f138f0969631b29ee429c44955c8f31cf744cc08
Gustavo Lozada <zelote57@gmail.com>
2024-01-23 22:32:16 -0500

    add entity framework

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicAPI.Models;
+
+namespace MusicAPI.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) 
+            : base(options)
+        {            
+        }
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
